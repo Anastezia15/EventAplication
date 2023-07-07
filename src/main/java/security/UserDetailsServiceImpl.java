9e418a6f4ec8fd70004b8f44479425a1e8bb40bb
agavriluta <agavriluta@griddynamics.com>
2023-07-07 17:51:36 +0300

    unfinished security

---
+++ src/main/java/security/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import user_management.UserRepository;
+import user_management.model.User;
+
+@Service("userDetailsServiceImpl")
+public class UserDetailsServiceImpl implements UserDetailsService {
+   UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Not found: " + username);
+        }
+
+        return UserDetailsImpl.fromUser(user);
+    }
+}
---
+++ src/main/java/user_management/model/Role.java
@@ -0,0 +1,19 @@
+package user_management.model;
+
+public enum Role {
+    ROLE_USER("ROLE_USER"),
+    ROLE_ADMIN("ROLE_ADMIN");
+
+    private final String roleName;
+
+   private Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+
+}
+
